880689b9404bdd461cd49cb8517f57b91119f8ad
JamarinoDopazo <janaina.jamarino@sga.pucminas.br>
2022-06-05 21:14:58 -0300

    iniciando react

--- src/sample/src/App.js
+++ src/sample/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>Veja os Indicados</code> e dê seu palpite.
         </p>
         <a
           className="App-link"
